f6356072dd7e0515a41334d1f24cf69bb99c6351
dandy-list-0183 <cmperks@yahoo.com>
2021-02-09 07:13:52 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,30 @@
-function basicTeenager(age) {
-
+function basicTeenager(age){ 
+ if (age >13 && age <19){
+    return "You are a teenager!";
+  }
 }
-
-function teenager(age) {
-
+function teenager(age){
+  if (age >13 && age <19){
+    return "You are a teenager!";
+  }else{
+return "You are not a teenager";
 }
-
+}
 function ageChecker(age) {
-
+if (age >13 && age <19){
+    return "You are a teenager!";
+}else if (age <12){
+  return "You are a kid";
+}else (age >19);
+{return "You are a grownup";
 }
-
+}
 function ternaryTeenager(age) {
-
+  return "You are a teenager"
+  if (age <13 && age >19 ){
+    return "You are not a teenager";
+  }
 }
-
 function switchAge(age) {
 
-}
+ }
